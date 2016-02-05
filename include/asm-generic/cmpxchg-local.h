e22c1a79ced0e7d97b8be2a579e74a4da16012d7
Darius Rad <darius@bluespec.com>
2016-02-05 18:01:29 -0500

    include/asm-generic/cmpxchg-local.h: perform comparison in cmpxchg using appropriate size of data
    In the generic implementation of cmpxchg, cast the parameters to the size
    of the data prior to comparison.  Otherwise, it is possible for the
    comparison to be done incorrectly in the case where the data size is
    smaller than the architecture register size.
    
    For example, on a 64-bit architecture, a 32-bit value is sign extended
    differently if it is typecast from an int to an unsigned long as compared
    to being loaded from memory via an unsigned pointer (u32 *).  Given that
    the primary, or possibly only, use of cmpxchg is with 4 and 8 byte values,
    the incorrect comparison could only occur on 64-bit architectures that make
    use of the generic cmpxchg.

--- include/asm-generic/cmpxchg-local.h
+++ include/asm-generic/cmpxchg-local.h
@@ -25,19 +25,19 @@ static inline unsigned long __cmpxchg_local_generic(volatile void *ptr,
 	raw_local_irq_save(flags);
 	switch (size) {
 	case 1: prev = *(u8 *)ptr;
-		if (prev == old)
+		if ((u8)prev == (u8)old)
 			*(u8 *)ptr = (u8)new;
 		break;
 	case 2: prev = *(u16 *)ptr;
-		if (prev == old)
+		if ((u16)prev == (u16)old)
 			*(u16 *)ptr = (u16)new;
 		break;
 	case 4: prev = *(u32 *)ptr;
-		if (prev == old)
+		if ((u32)prev == (u32)old)
 			*(u32 *)ptr = (u32)new;
 		break;
 	case 8: prev = *(u64 *)ptr;
-		if (prev == old)
+		if ((u64)prev == (u64)old)
 			*(u64 *)ptr = (u64)new;
 		break;
 	default:
