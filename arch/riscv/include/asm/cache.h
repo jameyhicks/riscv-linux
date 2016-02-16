3d97663effa263124499943f876e221429c19ba4
Darius Rad <darius@bluespec.com>
2016-02-16 16:05:57 -0500

    Add CONFIG_RV_L1_CACHE_SHIFT.
    Support configurable cache line size.  Fix size for Bluespec Flute
    cache line while we're at it.

--- arch/riscv/include/asm/cache.h
+++ arch/riscv/include/asm/cache.h
@@ -1,12 +1,7 @@
 #ifndef _ASM_RISCV_CACHE_H
 #define _ASM_RISCV_CACHE_H
 
-#if defined(CONFIG_CPU_RV_ROCKET) || defined(CONFIG_CPU_RV_BLUESPEC)
-#define L1_CACHE_SHIFT		6
-#else
-#define L1_CACHE_SHIFT		5
-#endif
-
+#define L1_CACHE_SHIFT		CONFIG_RV_L1_CACHE_SHIFT
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
 #endif /* _ASM_RISCV_CACHE_H */
